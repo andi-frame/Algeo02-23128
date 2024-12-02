48af55f036d8f5e28c8767c14e79411c3a8e0437
Azzkaaaa <19623121@std.stei.itb.ac.id>
2024-12-02 23:15:32 +0700

    feat: add data centering

---
+++ src/backend/data_centering.py
@@ -0,0 +1,23 @@
+from image_processing import turn_grayscale
+from image_processing import resize_image
+import numpy as np
+
+def data_centering(image_paths):
+    dataset = np.array([resize_image(turn_grayscale(image_path), (2, 2)) for image_path in image_paths])
+    N, m, n = dataset.shape
+
+    myu = np.zeros((m, n), dtype=float)
+    for row in range(m):
+        for col in range(n):
+            sum_pixel = 0
+            for i in range(N):
+                sum_pixel += dataset[i, row, col]
+            myu[row, col] = sum_pixel / N
+
+    standardized = np.zeros_like(dataset, dtype=float)
+    for i in range(N):
+        for row in range(m):
+            for col in range(n):
+                standardized[i, row, col] = dataset[i, row, col] - myu[row, col]
+
+    return standardized
