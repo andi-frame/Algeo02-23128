0ae17d79335792a90f20166b0800dea66e1463ce
andi-frame <andifarhan1094@gmail.com>
2024-12-06 18:27:25 +0700

    feat: add all color pallete

--- src/frontend/tailwind.config.ts
+++ src/frontend/tailwind.config.ts
@@ -1,16 +1,17 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "spotify-green": "#1db954",
+        "spotify-black-1": "#212121",
+        "spotify-black-2": "#121212",
+        "spotify-gray-1": "#535353",
+        "spotify-gray-2": "#b3b3b3",
       },
     },
   },
