6700e6b9425f7b768d6d5c4219a29df3fe43c66e
andi-frame <andifarhan1094@gmail.com>
2025-01-20 11:25:02 +0800

    Style

--- src/frontend/components/Tracks.tsx
+++ src/frontend/components/Tracks.tsx
@@ -6,7 +6,7 @@ const Tracks = () => {
   const tracks = useAlbumStore((state) => state.tracks);
   return (
     <div className="w-full flex flex-col justify-center items-center text-white cursor-pointer">
-      <div className="w-2/3 text-left text-2xl font-semibold ml-10 mb-5">Tracks Only For You</div>
+      <div className="w-2/3 text-left text-2xl font-semibold ml-10 mb-5">Tracks You Looking For {"(by Image)"}</div>
       <div className="w-2/3 justify-center items-center flex gap-5 flex-col">
         {tracks && tracks.map((track, index) => <TrackCard key={index} track={track} />)}
         {!tracks && <div>Error: No Data Returned</div>}
--- src/frontend/components/TracksHumming.tsx
+++ src/frontend/components/TracksHumming.tsx
@@ -6,7 +6,7 @@ const TracksHumming = () => {
   const tracks = useAlbumStore((state) => state.trackHummingList);
   return (
     <div className="w-full flex flex-col justify-center items-center text-white cursor-pointer">
-      <div className="w-2/3 text-left text-2xl font-semibold ml-10 mb-5">Tracks Only For You</div>
+      <div className="w-2/3 text-left text-2xl font-semibold ml-10 mb-5">Tracks You Looking For {"(by Humming)"}</div>
       <div className="w-2/3 justify-center items-center flex gap-5 flex-col">
         {tracks && tracks.map((track, index) => <TrackHummingCard key={index} track={track} />)}
         {!tracks && <div>Error: No Data Returned</div>}
